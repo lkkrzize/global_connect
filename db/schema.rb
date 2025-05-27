f6ecd949ee92ef6eec03e17deb77e6b556cfc0e1
Lukasz Krzizek <lukaszkrzi97@gmail.com>
2025-05-27 15:04:39 +0100

    added category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_135127) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_27_140344) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "event_users", force: :cascade do |t|
+    t.bigint "event_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_users_on_event_id"
+    t.index ["user_id"], name: "index_event_users_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -52,6 +67,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_135127) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "event_users", "events"
+  add_foreign_key "event_users", "users"
   add_foreign_key "events", "users", column: "users_id"
   add_foreign_key "messages", "events"
   add_foreign_key "messages", "users"
