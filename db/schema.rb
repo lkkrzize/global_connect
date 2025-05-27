c8a9582506f503fec071a8ad87fc9829215f38c7
Lukasz Krzizek <lukaszkrzi97@gmail.com>
2025-05-27 14:52:53 +0100

    added even_users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_114437) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_27_135127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.text "description"
+    t.integer "people_limit"
+    t.date "date"
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_events_on_users_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "event_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_messages_on_event_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_114437) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "users_id"
+  add_foreign_key "messages", "events"
+  add_foreign_key "messages", "users"
 end
