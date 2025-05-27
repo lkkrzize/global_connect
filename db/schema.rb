d0d13966edd631bb5942e710c066023e80beca3b
Lukasz Krzizek <lukaszkrzi97@gmail.com>
2025-05-27 15:25:18 +0100

    created review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_140501) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_27_142059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,17 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_140501) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.float "rating"
+    t.text "comment"
+    t.bigint "event_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_reviews_on_event_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -83,4 +94,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_140501) do
   add_foreign_key "events", "users", column: "users_id"
   add_foreign_key "messages", "events"
   add_foreign_key "messages", "users"
+  add_foreign_key "reviews", "events"
+  add_foreign_key "reviews", "users"
 end
