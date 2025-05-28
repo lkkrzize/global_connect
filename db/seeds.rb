bba30c3475529608eab465e1f11e5b15838215d0
Vytautas2025 <your_email@example.com>
2025-05-28 14:06:03 +0100

    seedfile finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,69 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-pust "Seeding database"
+puts "Cleaning the Database"
 
-User.create!
+EventUser.destroy_all
+EventCategory.destroy_all
+Review.destroy_all
+Event.destroy_all
+User.destroy_all
+Category.destroy_all
+
+puts "Seeding database"
+
+juan = User.create!(email: "juan@lewagon.com", password: "password")
+shwetha = User.create!(email: "shwetha@lewagon.com", password: "password")
+vytautas = User.create!(email: "vytautas@lewagon.com", password: "password")
+lukasz = User.create!(email: "lukasz@lewagon.com", password: "password")
+jessica = User.create!(email: "jessica@lewagon.com", password: "password")
+
+puts "users created"
+
+puts "Creating Events"
+
+event1 = Event.create!(name: "5 miles hike", location: "london", description: "easy hike", people_limit: 50, date: Date.new(2025, 5, 30), user_id: juan.id )
+event2 = Event.create!(name: "pub crawl", location: "berlin", description: "going to pubs", people_limit: 250, date: Date.new(2025, 6, 11), user_id: shwetha.id)
+event3 = Event.create!(name: "marathon", location: "madeira", description: "running", people_limit: 100, date: Date.new(2025, 8, 24), user_id: vytautas.id)
+event4 = Event.create!(name: "JS event", location: "paris", description: "master JS", people_limit: 500, date: Date.new(2025, 10, 02), user_id: lukasz.id)
+event5 = Event.create!(name: "car show", location: "crete", description: "supercars", people_limit: 900, date: Date.new(2025, 12, 31), user_id: jessica.id )
+
+puts "Events created"
+
+puts "Creating categories"
+
+category1 = Category.create!(name: "Outdoor Activities")
+category2 = Category.create!(name: "Social Events")
+category3 = Category.create!(name: "Learning")
+
+puts "Categories created"
+
+
+puts "Assigning categories to events"
+
+EventCategory.create!(event_id: event1.id, category_id: category1.id)
+EventCategory.create!(event_id: event2.id, category_id: category2.id)
+EventCategory.create!(event_id: event3.id, category_id: category1.id)
+EventCategory.create!(event_id: event4.id, category_id: category3.id)
+EventCategory.create!(event_id: event5.id, category_id: category2.id)
+
+puts "Event categories assigned"
+
+puts "Creating event users"
+
+EventUser.create!(user_id: juan.id, event_id: event1.id)
+EventUser.create!(user_id: lukasz.id, event_id: event1.id)
+EventUser.create!(user_id: vytautas.id, event_id: event2.id)
+EventUser.create!(user_id: jessica.id, event_id: event2.id)
+EventUser.create!(user_id: shwetha.id, event_id: event3.id)
+
+
+puts "Event users created"
+
+puts "Creating reviews"
+
+Review.create!(rating: 4, comment: "Great hike!", user_id: juan.id, event_id: event1.id)
+Review.create!(rating: 3, comment: "Fun pub crawl.", user_id: jessica.id, event_id: event2.id)
+Review.create!(rating: 5.0, comment: "JS event was well organised!", user_id: lukasz.id, event_id: event4.id)
+
+puts "Reviews created"
