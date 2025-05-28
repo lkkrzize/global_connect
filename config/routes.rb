d02c3b483da5d2481480dbe3c5b93e2cf9c2cf93
Lukasz Krzizek <lukaszkrzi97@gmail.com>
2025-05-28 15:57:59 +0100

    edit form completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  # resources :events, only: %i[edit update] do
+  # resources :events do
+
   resources :events do
     # resources :messages, only: []
     # resources :event_users, only: []
