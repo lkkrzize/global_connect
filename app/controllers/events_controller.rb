06d7d0497058e88c6e14aa03a334cc82d49b00ca
Vytautas2025 <your_email@example.com>
2025-05-28 17:46:58 +0100

    Show page in progress

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
   def show
+    @event = Event.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'events/show'
   devise_for :users
   root to: "pages#home"
 
-  resources :events, only: [] do
+  resources :events, only: [:show] do
     # resources :messages, only: []
     # resources :event_users, only: []
     # resources :reviews, only: []
