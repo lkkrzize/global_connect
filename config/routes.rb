b6b7dfeb46741d4932a27eb30184da6f1eaff831
Vytautas2025 <your_email@example.com>
2025-05-29 11:26:20 +0100

    routes edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'event_users/create'
-  get 'event_users/destroy'
   devise_for :users
   root to: "events#index"
-  
+
   resources :events do
     # resources :messages, only: []
     resources :event_users, only: [:create, :destroy]
