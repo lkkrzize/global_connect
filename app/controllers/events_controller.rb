1357720e0b0973c2bfa28158b9500f51d91a8f97
Vytautas2025 <your_email@example.com>
2025-05-29 08:24:53 +0100

    added register button logic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,10 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def register
+    @event = Event.find(params[:id])
+    @event.event_users.create(user: current_user)
+    redirect_to @event
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'event_users/create'
+  get 'event_users/destroy'
   devise_for :users
   root to: "pages#home"
 
   resources :events, only: [:show] do
     # resources :messages, only: []
-    # resources :event_users, only: []
+    resources :event_users, only: [:create, :destroy]
     # resources :reviews, only: []
   end
 end
