5c08ab58bd7821c843e3ccde7ae80103270d6a09
Shwetha Marumada <shwe.nie@gmail.com>
2025-05-30 14:05:37 +0100

    real time messaging implemented

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,15 @@
 class Message < ApplicationRecord
   belongs_to :event
   belongs_to :user
+
+  after_create_commit :broadcast_message
+
+  private
+
+  def broadcast_message
+    broadcast_append_to "event_#{event.id}_messages",
+                        partial: "messages/message",
+                        target: "messages",
+                        locals: { message: self }
+  end
 end
