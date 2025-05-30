0b99ead8b01ca7fe303764048be6f1e490c336e8
Lukasz Krzizek <lukaszkrzi97@gmail.com>
2025-05-30 13:24:51 +0100

    make the event-banner_image accessible everywhere

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
   def pages_home
     'landing'
   end
+
+  def event_banner_image(event)
+    # checking if photo attached to stop page crashing and use placeholder if no image attached
+    if event.photo.attached?
+      cl_image_path(event.photo.key)
+    else
+      "https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/background.png"
+    end
+  end
+  
 end
