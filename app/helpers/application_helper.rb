dedea4854c3b9b211fd23694197dfa87dbe80b2c
Lukasz Krzizek <lukaszkrzi97@gmail.com>
2025-05-30 13:29:45 +0100

    Update application_helper.rb
    moved comment two lines up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
   def pages_home
     'landing'
   end
-
+  
+  # checking if photo attached to stop page crashing and use placeholder if no image attached
   def event_banner_image(event)
-    # checking if photo attached to stop page crashing and use placeholder if no image attached
     if event.photo.attached?
       cl_image_path(event.photo.key)
     else
