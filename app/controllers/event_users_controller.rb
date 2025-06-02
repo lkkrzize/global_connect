b616c10414ca40a55e0b3957fcd48b9301216d05
Vytautas2025 <vytautas.briauka@gmail.com>
2025-06-02 10:47:58 +0100

    Update event_users_controller.rb

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -3,7 +3,7 @@ before_action :authenticate_user!
 
   def create
     @event = Event.find(params[:event_id])
-    @event.event_users.create!(user: current_user)
+    @event.event_users.create(user: current_user)
 
     redirect_to @event
   end
