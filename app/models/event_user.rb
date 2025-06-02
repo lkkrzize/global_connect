45f72c394184834498f8a2faa7e43c9b8651b317
Lukasz Krzizek <lukaszkrzi97@gmail.com>
2025-06-02 15:09:18 +0100

    fixed validations for registering users and re-done seed_file

--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -2,5 +2,6 @@ class EventUser < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  validates :user_id, uniqueness: true
+  validates :user_id, uniqueness: { scope: :event_id,
+    message: "cannot attend event more than once" }
 end
