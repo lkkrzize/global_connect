59c867983ab5095ffa40b1604af597e8131022c7
Lukasz Krzizek <lukaszkrzi97@gmail.com>
2025-06-03 10:32:58 +0100

    fixed css

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,11 @@
 module EventsHelper
   def user_registered?(event, user)
-    event.users.exists?(user.id)
+    event.event_users.exists?(user: user)
   end
+
+  def event_owner?(event, user)
+    event.user == user
+  end
+
 end
+
