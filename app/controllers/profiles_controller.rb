96575da6fa76065453ba84d125216e6c933e1fd3
Lukasz Krzizek <lukaszkrzi97@gmail.com>
2025-06-03 16:33:17 +0100

    Update profiles_controller.rb
    removed duplciate user_params private method

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -42,10 +42,4 @@ class ProfilesController < ApplicationController
       render :show, alert: 'Failed to update profile.'
     end
   end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :bio)
-  end
 end
