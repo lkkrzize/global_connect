029d982e644cfe9bfe5c992e870880228f16e1c3
Vytautas2025 <your_email@example.com>
2025-06-04 12:52:11 +0100

    images in progress

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -7,5 +7,13 @@ module EventsHelper
     event.user == user
   end
 
+  def event_banner_image(event)
+    if event.photo.attached?
+      url_for(event.photo)
+    else
+      # Fallback to a consistent random image from Picsum for this event
+      # if event.photo is not attached and event.image_url was removed.
+      "https://picsum.photos/seed/#{event.description}/600/400"
+    end
+  end
 end
-
