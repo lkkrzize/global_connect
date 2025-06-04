3ca7327a2e39aefc238aeec121be36b49006e8c3
Vytautas2025 <your_email@example.com>
2025-06-04 18:02:18 +0100

    added images to the events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -10,10 +10,10 @@ module EventsHelper
   def event_banner_image(event)
     if event.photo.attached?
       url_for(event.photo)
+    elsif event.image_url.present?
+      event.image_url
     else
-      # Fallback to a consistent random image from Picsum for this event
-      # if event.photo is not attached and event.image_url was removed.
-      "https://picsum.photos/seed/#{event.name}/600/400"
+      "https://picsum.photos/seed/#{event.id}/600/400"
     end
   end
 end
