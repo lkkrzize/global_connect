91e8f7e5d46d88566dbb12dc3578ad6b191fb37b
Lukasz Krzizek <lukaszkrzi97@gmail.com>
2025-06-04 17:37:58 +0100

    pg_search gem added and in events controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,15 @@ class Event < ApplicationRecord
   has_many :users, through: :event_users
   has_many :messages, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  
+
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+
+
+  include PgSearch::Model
+  pg_search_scope :search_by_events_and_location,
+  against: [ :title, :synopsis ],
+  using: {
+    tsearch: { prefix: true } 
+  }
 end
